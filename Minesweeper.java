b8f5e671053d7e8c08c8242248ff95691d7b9e8c
Luke Montgomery <luke.p.montgomery@gmail.com>
2018-11-21 17:57:53 +0000

    first commit

---
+++ Minesweeper.java
@@ -0,0 +1,31 @@
+package minesweeper;
+
+
+public class Minesweeper {
+	
+	public static void main(String[] args) {
+		placeMines();
+		
+	}
+	
+	public static void placeMines() {
+		
+		int[][] mineField = new int[10][10];
+		
+		// 1 means mine, 0 means no mine
+		for (int i = 0; i < mineField.length; i++) {
+			System.out.println("");
+			for (int j = 0; j < mineField.length; j++) {
+				
+				int randomI = (int) (Math.random()*10);
+				int randomJ = (int) (Math.random()*10);
+				mineField[randomI][randomJ] = 1;
+				
+				System.out.print("[" + mineField[i][j] + "]");
+			}
+			
+		}
+		
+	}
+	
+}
